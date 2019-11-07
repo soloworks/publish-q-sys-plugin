5fd69ddb72c0add0508f4c330e745900496d93c8
Sam Shelton <sam.shelton@soloworks.co.uk>
2019-11-07 19:22:43 +0000

    Added MarkDown file option with image path conversion

--- README.md
+++ README.md
@@ -18,7 +18,15 @@ The API Key that grants write access to the NuGet server
 
 ### `version`
 
-The version to tag this release as. Defaults to 'commit-tag' which will try to extract it from the commit tag.
+The version to tag this release as in NuSpec file. Defaults to 'commit-tag' which will try to extract it from the commit tag. Will replace any instances of `0.0.0.0-master` in .qplug files with version. Must be in format `x.x.x.x`.
+
+### `release-notes`
+
+Gathers git commit references since last version tag and replaces ReleaseNotes field of NuSpec file.
+
+### `md-to-desc`
+
+Converts specified MarkDown file to NuGet server format and replaces Description field of NuSpec file. Release notes can be included using `[[RELEASE-NOTES]]`
 
 ## Outputs
 
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -32,7 +33,8 @@ func main() {
 	if ver == "" {
 		log.Fatalln("No version supplied or found ($GITHUB_REF="+os.Getenv("GITHUB_REF"), ")")
 	}
-	// Check it against RegEx to be in format x.x.x.x only
+
+	// Check ver against RegEx to be in format x.x.x.x only
 	if match, _ := regexp.MatchString(`^(([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+))$`, ver); !match {
 		log.Fatalln("SemVer format incorrect: ", ver)
 	}
@@ -52,6 +54,11 @@ func main() {
 		}
 	}
 
+	// If no NuSpec is found, exit with error
+	if nsfilename == "" {
+		log.Fatalln("No NuSpec supplied or found")
+	}
+
 	// Load the NuSpec file
 	nsf, err := nuspec.FromFile(nsfilename)
 	if err != nil {
@@ -62,7 +69,6 @@ func main() {
 	nsf.Meta.Version = ver
 
 	// Edit all .qplug files to update version from 0.0.0.0-master
-
 	m, err := filepath.Glob("content/*.qplug")
 	if err != nil {
 		log.Fatalln(err)
@@ -77,25 +83,46 @@ func main() {
 		if err != nil {
 			log.Fatalln(err)
 		}
-
 	}
+
+	// Gather Git Release notes
+
+	// Convert MarkDown to Description
+	if x := a.GetInput("md-to-desc"); x != "" {
+		// Load the file
+		read, err := ioutil.ReadFile(x)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// Do conversion to NuSpec formats - Images
+		reImg := regexp.MustCompile(`(?:!\[(.*?)\]\((.*?)\))`)
+		reBraces := regexp.MustCompile(`\((.*)\)`)
+		nsf.Meta.Description = reImg.ReplaceAllStringFunc(string(read), func(s string) string {
+			log.Println(s)
+			return reBraces.ReplaceAllStringFunc(string(s), func(s string) string {
+				p := path.Clean(s[1 : len(s)-1])
+				return "(http://" + p + ")"
+			})
+		})
+	}
+	log.Println(nsf.Meta.Description)
 	// Pack it up
-	npkg, err := nuget.PackNupkg(nsf, ".", ".")
+	_, err = nuget.PackNupkg(nsf, ".", ".")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// Push it up
-	status, _, err := nuget.PushNupkg(npkg, a.GetInput("Api-Key"), a.GetInput("nuget-host"))
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// // Push it up
+	// status, _, err := nuget.PushNupkg(npkg, a.GetInput("Api-Key"), a.GetInput("nuget-host"))
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
 
-	if status == 201 {
-		os.Exit(0)
-	} else {
-		log.Fatalln("Failed with HTTP Status:", status)
-	}
+	// if status == 201 {
+	// 	os.Exit(0)
+	// } else {
+	// 	log.Fatalln("Failed with HTTP Status:", status)
+	// }
 
 	// Set Output variables
 	//println("::set-output name=duration::", strconv.Itoa(dur))
